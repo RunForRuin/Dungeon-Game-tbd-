fac020202205f4e8ad345134abb5dc2bba7df3d9
RunForRuin <167572327+RunForRuin@users.noreply.github.com>
2025-02-25 10:14:10 -0600

    Added counter/total
    Added a counter for the array containing each die roll as well as a total. (Also a few commands that print to the console)

--- dungame/Game/Scripts/dice.gd
+++ dungame/Game/Scripts/dice.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
+var roll_array
+
 var dice
+var roll_amount
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	get_parent().dice_phase.connect(roll)
+	get_tree().current_scene.dice_phase.connect(roll)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,7 +15,10 @@ func _process(delta: float) -> void:
 	
 
 func roll() -> void:
+	roll_array = get_parent().dice_rolls
 	dice = randi_range(0,5)
-	print("I WORK: ", dice + 1)
+	roll_amount = dice + 1
+	print("I WORK: ", roll_amount)
 	$"Area2D/Dice sides".set_frame(dice)
+	roll_array.append(roll_amount)
 	pass
--- dungame/Game/Scripts/game_controls.gd
+++ dungame/Game/Scripts/game_controls.gd
@@ -1,9 +1,5 @@
 extends Node2D
 
-const dice_path = preload("res://Game/Scenes/dice.tscn") #the path for the dice (to instantiate)
-
-var dice_number = 0
-
 #variables for phases and turns
 var phases = 3 #sets the number of phases
 var phase_num = 0
@@ -25,8 +21,6 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if (Input.is_action_just_pressed("Change turn")): #possibly delete later - depends on other phases
 		emit_signal("next_turn")
-	if Input.is_action_just_pressed("Spawn Dice (temporary)"):
-		spawn_dice()
 	pass
 	
 
@@ -52,10 +46,3 @@ func Next_Turn() -> void:
 func _on_next_turn() -> void:
 	Next_Turn()
 	print("turn ",turn,", ","phase ",phase_num,": ", phase)
-
-func spawn_dice() -> void:
-	dice_number += 1
-	var dice = dice_path.instantiate()
-	get_tree().current_scene.add_child(dice)
-	dice.position = get_local_mouse_position()
-	print("Dice #",dice_number, " spawned B)")
