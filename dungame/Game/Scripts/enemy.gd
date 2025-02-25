2e80b82528e5508d4f8901d217b64a84b3e720c8
RunForRuin <167572327+RunForRuin@users.noreply.github.com>
2025-02-25 01:56:49 -0600

    Updated dice spawning
    You can now press 1 to spawn a die at the mouse position. Press space to change "phases"

--- dungame/Game/Scripts/enemy.gd
+++ dungame/Game/Scripts/enemy.gd
@@ -1,18 +1,11 @@
 extends Node2D
 
-var dice
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	get_parent().dice_phase.connect(roll)
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-	
-
-func roll() -> void:
-	dice = randi_range(0,5)
-	print("I WORK: ", dice + 1)
-	$"Area2D/Dice sides".set_frame(dice)
 	pass
--- dungame/Game/Scripts/game_controls.gd
+++ dungame/Game/Scripts/game_controls.gd
@@ -1,11 +1,20 @@
 extends Node2D
 
-signal next_turn
-signal roll_dice
+const dice_path = preload("res://Game/Scenes/dice.tscn") #the path for the dice (to instantiate)
+
+var dice_number = 0
+
+#variables for phases and turns
 var phases = 3 #sets the number of phases
 var phase_num = 0
 var phase
 var turn = 1
+#signals for phases and turns
+signal next_turn #may not end up being useful
+signal dice_phase
+signal enemy_phase
+signal card_phase
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,11 +23,14 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if (Input.is_action_just_pressed("Change turn")):
+	if (Input.is_action_just_pressed("Change turn")): #possibly delete later - depends on other phases
 		emit_signal("next_turn")
-		
+	if Input.is_action_just_pressed("Spawn Dice (temporary)"):
+		spawn_dice()
 	pass
 	
+
+#controls the turns/phases - 3 phases per turn (card, dice, enemy)
 func Next_Turn() -> void:
 	if phase_num < phases:
 		phase_num += 1
@@ -28,13 +40,22 @@ func Next_Turn() -> void:
 	
 	if phase_num == 1:
 		phase = "Card Phase"
+		emit_signal("card_phase")
 	elif phase_num == 2:
 		phase = "Dice Phase"
-		emit_signal("roll_dice")
+		emit_signal("dice_phase")
 	elif phase_num == 3:
 		phase = "Enemy Phase"
+		emit_signal("enemy_phase")
 
 # When signal "next_turn" is emitted
 func _on_next_turn() -> void:
 	Next_Turn()
 	print("turn ",turn,", ","phase ",phase_num,": ", phase)
+
+func spawn_dice() -> void:
+	dice_number += 1
+	var dice = dice_path.instantiate()
+	get_tree().current_scene.add_child(dice)
+	dice.position = get_local_mouse_position()
+	print("Dice #",dice_number, " spawned B)")
